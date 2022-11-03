ce72fe64dddec31b69843be5d599705cd416b5a9
ndubuisiazi <106999600+ndubuisiazi@users.noreply.github.com>
2022-11-02 21:10:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,41 +9,42 @@ const req = require('express/lib/request')
 
 const port = process.env.PORT || 3001
 
-// app.use(express.static("public"))
+app.use(express.static("public"))
 
-// app.get('/api/notes',(req,res)=>{
-//     res.sendFile('/Users/ndubuisiazi/Desktop/Bootcamp/Challenge 11/11/public/api/db.json')
-//     // res.send('/Users/ndubuisiazi/Desktop/Bootcamp/Challenge 11/11/public/api/db.json','utf-8')
-// })
+app.get('/api/notes',(req,res)=>{
+    res.sendFile('/Users/ndubuisiazi/Desktop/Bootcamp/Challenge 11/11/public/api/db.json')
+    // res.send('/Users/ndubuisiazi/Desktop/Bootcamp/Challenge 11/11/public/api/db.json','utf-8')
+})
 
-// app.use(express.json());
-// app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-// app.post('/api/notes',(req,res)=>{
-//     let id =uuidv4()
-//     noteList.push(
-//         {"title":req.body.title,
-//         "text":req.body.text,
-//         "id":id})
-//         app.delete('/api/notes/'+id,(res,req)=>{
-// console.log(req)
-//         })
-//     console.log(id)
-//     console.log(noteList)
-
-
-//     fs.writeFileSync("./public/api/db.json",JSON.stringify(noteList))
-//     res.end()
-// })
+app.post('/api/notes',(req,res)=>{
+    let id =uuidv4()
+    noteList.push(
+        {"title":req.body.title,
+        "text":req.body.text,
+        "id":id})
+        app.delete('/api/notes/'+id,(res,req)=>{
+console.log(req)
+        })
+    console.log(id)
+    console.log(noteList)
 
 
-// app.get('/notes',(req,res)=>{
-//     res.sendFile('/Users/ndubuisiazi/Desktop/Bootcamp/Challenge 11/11/public/notes.html')
-// })
+    fs.writeFileSync("./public/api/db.json",JSON.stringify(noteList))
+    res.end()
+})
 
-// app.post('/notess',(req,res)=>{
-//     res.sendFile('/Users/ndubuisiazi/Desktop/Bootcamp/Challenge 11/11/public/notes.html')
-// })
+
+app.get('/notes',(req,res)=>{
+    // res.sendFile('/Users/ndubuisiazi/Desktop/Bootcamp/Challenge 11/11/public/notes.html')
+    res.send('test')
+})
+
+app.post('/notess',(req,res)=>{
+    res.sendFile('/Users/ndubuisiazi/Desktop/Bootcamp/Challenge 11/11/public/notes.html')
+})
 
 
    
